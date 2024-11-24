79223ac4acc67ac1de98447929bceeb99b463dec
Anshul Sharma <ianshulsharma26@gmail.com>
2024-11-24 21:58:25 +0530

    understand evenet listener with clouser

---
+++ removeDuplicates.js
@@ -0,0 +1,7 @@
+let stringArray = ['duplicates', 'Apple', 'higher', 'Apple', 'higher', 'vicky', 'Vicky', 'Karishma'];
+
+// let filterArray = [...new Map(stringArray.map(item => [item.toLowerCase(), item])).values()];
+// let filterArray = [...new Object(stringArray.map(item => [item.toLowerCase(), item])).values()];
+
+let myArray = new Map(['p','1'])
+console.log(myArray);
